8891628fd5898b6f4edb010762e9e66e5feb7915
Facundo <facundo.cba2@gmail.com>
2021-01-15 20:38:33 -0300

    git init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let numero = [1 ,2 ,3 ,4 ,5];
+
+	function addNumero() {
+		numero =[...numero , numero.length+1];
+	}
+
+	$: suma= numero.reduce((a ,b) => a + b ,0);
+
+  </script>
+
+  <p>{numero.join(" * ")} = {suma}</p>
+  <button on:click={addNumero}>Poner 1 numero mas</button>
+
+
+
+
+
+
+	
+ 
